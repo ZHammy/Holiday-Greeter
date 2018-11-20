66e31b925e5684f145bb6ce67a81e3c4ab7be3e8
Zach Hammitt <hammitzt@mail.uc.edu>
2018-11-20 13:22:20 -0500

    Got all the good stuff here

---
+++ Holiday/Holiday/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Holiday
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.presentButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // presentButton
+            // 
+            this.presentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.presentButton.Image = ((System.Drawing.Image)(resources.GetObject("presentButton.Image")));
+            this.presentButton.Location = new System.Drawing.Point(234, 115);
+            this.presentButton.Name = "presentButton";
+            this.presentButton.Size = new System.Drawing.Size(338, 228);
+            this.presentButton.TabIndex = 0;
+            this.presentButton.Text = "Open Gift";
+            this.presentButton.UseVisualStyleBackColor = true;
+            this.presentButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(251, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(621, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(234, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(338, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Merry Christmas!";
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(251, 282);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(297, 120);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Test Message";
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(265, 418);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(265, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Check your spam if you\'re not seeing anything";
+            this.textBox3.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Enabled = false;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(34, 133);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.presentButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button presentButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Holiday/Holiday/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace Holiday
+{
+    public partial class Form1 : Form
+    {
+        //Constants and tutorial
+        //In order to customize any of the text in the program window itsef, you should do that in the designer
+        //You can also change the images too that way
+
+        //Put the earliest date that the Gift can be sent in the format MM/dd/yyyy
+        string date = "12/25/2018";
+        //Put the name of the holiday
+        string holiday = "Christmas";
+        //Name of recipient to be dispayed
+        string recipient = "Test";
+        //Email of Recipient
+        string recipientEmail = "";
+        //Subject of Email
+        string emailSubject = "HolidayTest";
+        //Email text
+        string emailText = "Test" + System.Environment.NewLine+"Hello there" + System.Environment.NewLine + "General Kenobi" + "A bunch of test text that is just testing if I can test the line overflow with testing it but also testing how the email client in c# works, oh jeez I sure do love testing huh";
+        //Email Attatchment, give it a local file path
+        string attatchment = "attatchment.png";
+        //Sender Email
+        string senderEmail = "";
+        //Sender Password
+        string senderPassword = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Check if today is before the holiday date
+            if (DateTime.Today < DateTime.ParseExact(date, "MM/dd/yyyy", CultureInfo.InvariantCulture))
+            {
+                //If so, display a message that the recipient is trying to open early
+                MessageBox.Show(recipient + ", I know you are trying to open this early you goof" + System.Environment.NewLine + "You gotta wait till " + date);
+            }
+            //IF its past the holiday but the user can't connect to the internet, tell them they can't recieve the gift
+            else if (!checkInternetStatus())
+            {
+                MessageBox.Show("Hey hey, looks like you're not connected to the internet. "+System.Environment.NewLine+"This present gifter needs email to work, try again once you are online.  ");
+            }
+            //Else its on or past the date and they can open it
+            else
+            {
+                try
+                {
+                    //First disable and hide the button and enable everything else
+                    presentButton.Enabled = false;
+                    presentButton.Visible = false;
+                    pictureBox1.Enabled = true;
+                    pictureBox1.Visible = true;
+                    pictureBox2.Enabled = true;
+                    pictureBox2.Visible = true;
+                    pictureBox3.Enabled = true;
+                    pictureBox3.Visible = true;
+                    textBox1.Enabled = true;
+                    textBox1.Visible = true;
+                    textBox2.Enabled = true;
+                    textBox2.Visible = true;
+                    textBox3.Enabled = true;
+                    textBox3.Visible = true;
+
+                    using (MailMessage emailMessage = new MailMessage())
+                    {
+                        emailMessage.From = new MailAddress(senderEmail, senderPassword);
+                        emailMessage.To.Add(new MailAddress(recipientEmail));
+                        emailMessage.Subject = emailSubject;
+                        emailMessage.Body = emailText;
+                        emailMessage.Attachments.Add(new Attachment(attatchment));
+                        emailMessage.Priority = MailPriority.Normal;
+                        using (SmtpClient MailClient = new SmtpClient("smtp.gmail.com", 587))
+                        {
+                            MailClient.EnableSsl = true;
+                            MailClient.UseDefaultCredentials = false;
+                            MailClient.Credentials = new System.Net.NetworkCredential(senderEmail, senderPassword);
+                            MailClient.Send(emailMessage);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("An error occured.  Complain to the guy who programmed or sent you this and send him this"+System.Environment.NewLine+ System.Environment.NewLine + System.Environment.NewLine + ex.Message.ToString());
+                }
+                
+
+            }
+        }
+
+        private bool checkInternetStatus()
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                PingReply myReply = myPing.Send("Google.com");
+                return (myReply.Status == IPStatus.Success);
+            }
+            catch(Exception ex)
+            {
+                //Don't really care, it means we can't reach google anyways 
+            }
+            return false;
+            
+
+        }
+    }
+}
